0327cbfe0cded88851b637ff0410d5e106a5d10c
oxbowbet <118590025+oxbowbet@users.noreply.github.com>
2023-01-23 06:31:17 -0800

    Add files via upload
    Signed-off-by: oxbowbet <118590025+oxbowbet@users.noreply.github.com>

--- update.js
+++ update.js
@@ -2,22 +2,22 @@ let Update='\
 <h1>Sports Updates</h1>\
 <p>\
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, iste possimus repellendus veniam eius minus totam voluptas quam? Nobis dicta possimus maiores inventore minus ipsam aut explicabo iste doloribus nam.\
-    <img id="img" src="D:\\Pictures\\19998.jpg">\
+    <img id="img" src="https://drive.google.com/uc?export=download&id=1CEbUEdeJynW8r4djTzbmTRBaH3zKQhVf">\
 </p>\
 <p>\
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, iste possimus repellendus veniam eius minus totam voluptas quam? Nobis dicta possimus maiores inventore minus ipsam aut explicabo iste doloribus nam.\
-    <img id="img" src="D:\\Pictures\\19998.jpg">\
+    <img id="img" src="https://drive.google.com/uc?export=download&id=1CEbUEdeJynW8r4djTzbmTRBaH3zKQhVf">\
 </p>\
 <p>\
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, iste possimus repellendus veniam eius minus totam voluptas quam? Nobis dicta possimus maiores inventore minus ipsam aut explicabo iste doloribus nam.\
-    <img id="img" src="D:\\Pictures\\19998.jpg">\
+    <img id="img" src="https://drive.google.com/uc?export=download&id=1CEbUEdeJynW8r4djTzbmTRBaH3zKQhVf">\
 </p>\
 <p>\
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, iste possimus repellendus veniam eius minus totam voluptas quam? Nobis dicta possimus maiores inventore minus ipsam aut explicabo iste doloribus nam.\
-    <img id="img" src="D:\\Pictures\\19998.jpg">\
+    <img id="img" src="https://drive.google.com/uc?export=download&id=1CEbUEdeJynW8r4djTzbmTRBaH3zKQhVf">\
 </p>\
 <p>\
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, iste possimus repellendus veniam eius minus totam voluptas quam? Nobis dicta possimus maiores inventore minus ipsam aut explicabo iste doloribus nam.\
-    <img id="img" src="D:\\Pictures\\19998.jpg">\
+    <img id="img" src="https://drive.google.com/uc?export=download&id=1CEbUEdeJynW8r4djTzbmTRBaH3zKQhVf">\
 </p>\
 '
